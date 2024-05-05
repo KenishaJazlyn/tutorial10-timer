e9a96d572c3f3d3e7ecf4b3b65938235831c9aa3
KenishaJazlyn <kenishajazlyn@gmail.com>
2024-05-05 20:09:32 +0700

    Experiment 1.2: Understanding how it works

---
+++ README.md
@@ -0,0 +1,9 @@
+Understanding how it works
+
+![alt text](image.png)
+The async function runs concurrently with the main function, allowing "hey hey" to be printed first as it is executed outside the async function while the async function is still processing the future.
+
+
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -104,6 +104,7 @@ fn main() {
         TimerFuture::new(Duration::new(2, 0)).await;
         println!("Kenisha's PC: done!");
     });
+    println!("Kenisha's PC: hey hey");
 
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
